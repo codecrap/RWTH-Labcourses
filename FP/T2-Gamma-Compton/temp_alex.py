d348d6339d1bed16df32dc7e28748ad28395f969
aldrou <alexandre.drouet.96@gmail.com>
2019-02-23 16:17:45 +0100

    AlexTemp(Peakfinder)Update

--- FP/T2-Gamma-Compton/temp_alex.py
+++ FP/T2-Gamma-Compton/temp_alex.py
@@ -9,26 +9,35 @@ import numpy as np
 from matplotlib import pyplot as plt
 from scipy.optimize import curve_fit
 
-data = np.genfromtxt("Data/Eu_calibration.TKA")
-lt = data[0]
-rt = data[1]
-data = np.delete(data, 1)
-data = np.delete(data, 0)
-
-noise = np.genfromtxt("Data/Noise_calibration.TKA")
-noise = np.delete(noise, 1)
-noise = np.delete(noise, 0)
-
-count = data - noise
-
-plt.plot(count,".")
-
-chan = np.array(range(len(count)))
-
 def gauss(x, x0, sigma, a):
     return a * np.exp(-(x-x0)**2/(2.*sigma**2))
 
-opt, cov = curve_fit(gauss, chan, count, p0=[940,5,1])
+Cs = [[400,490]]
+Na = [[810,890]]
+Co = [[740,810], [850,910]]
+Eu = [[85,105], [160,195], [210,270], [480,560], [610,680], [690,780], [890,980]]
+
+noise = np.genfromtxt("Data/Noise_calibration.TKA")
+noise = np.delete(noise, [0,1])
+
+data = np.genfromtxt("Data/Eu_calibration.TKA")
+lt = data[0]
+rt = data[1]
+
+count = np.delete(data, [0,1])
+count = count - noise
+
+plt.plot(count,".")
+
+[before, peak, after] = np.split(count, Eu[3])
+
+chan = np.array(range(len(count)))
+[before, seg, after] = np.split(chan, Eu[3])
+
+plt.plot(seg, peak,".")
+
+
+opt, cov = curve_fit(gauss, seg, peak, p0=[Eu[3][0],1,1])
 mean = opt[0]
 dmean = np.sqrt(cov[0][0])
 sig = opt[1]
