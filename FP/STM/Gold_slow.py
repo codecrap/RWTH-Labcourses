9cb2547599e3fea0604f0e4aa271af436895345f
aldrou <alexandre.drouet.96@gmail.com>
2019-03-26 22:05:01 +0100

    STM gold report progress

--- FP/STM/Gold_slow.py
+++ FP/STM/Gold_slow.py
@@ -25,15 +25,31 @@ pl = reload(pl)
 
 # set strings and choose file
 DATAPATH = "./Gwyddion/Gold/"
-#FILE = "GAIN_opt_profile.txt"
-#FILE = "GAIN_slow_profile.txt"
-#FILE = "TIME_01_profile.txt"
-FILE = "TIME_005_profile.txt"
+FILE_PREFIX = ".txt"
+FILE_NAME = "GAIN_opt_profile"
+#FILE_NAME = "GAIN_slow_profile"
+#FILE_NAME = "TIME_01_profile"
+#FILE_NAME = "TIME_005_profile"
 
 # read data
-with open(DATAPATH + FILE) as data:
+with open(DATAPATH + FILE_NAME + FILE_PREFIX) as data:
 		   vForwardX, vForwardY, vBackwardX, vBackwardY = np.genfromtxt((line.replace(',', '.') for line in data), skip_header=3).T
 
-
-diff = np.abs(vForwardX[np.argmin(vForwardY)] - vBackwardX[np.argmin(vBackwardY)])
+# calculate difference between minima
+minF = vForwardX[np.argmin(vForwardY)]
+minB = vBackwardX[np.argmin(vBackwardY)]
+diff = np.abs(minF - minB)
 print(diff)
+
+# plot
+fig, ax = plt.subplots()
+#ax.set_title()
+ax.set_xlabel('x [m]')
+ax.set_ylabel('y [m]')
+ax.plot(vForwardX, vForwardY, 'b-', label='forward')
+ax.axvline(minF, color='b', linestyle='--', linewidth=1)
+ax.plot(vBackwardX, vBackwardY, 'k-', label='backward')
+ax.axvline(minB, color='k', linestyle='--', linewidth=1)
+ax.legend(loc='upper right')
+fig.savefig('Figures/'+FILE_NAME+'.pdf')
+
